4e806accc07f1da243c9896292dbb8c1079d62df
Jordan Bryan <jbryan382@gmail.com>
2019-02-18 23:15:36 -0500

    Played with IOTD further, and further mapped carosel

--- public/main.js
+++ public/main.js
@@ -1,3 +1,81 @@
-const main = () => {}
+// 'https://sdg-astro-api.herokuapp.com/api/'
 
-document.addEventListener('DOMContentLoaded', main)
+class updatePage {
+  constructor() {
+    this.status = {
+      launches: {
+        upcoming: [],
+        currentIndex: 0,
+        countdown: null
+      },
+      imageOfTheDay: {
+        copyright: 'loading',
+        title: 'loading',
+        url: ''
+      }
+    }
+  }
+
+  loadPage() {
+    this.loadImageOfTheDay()
+  }
+  loadImageOfTheDay() {
+    fetch('https://sdg-astro-api.herokuapp.com/api/Nasa/apod')
+      .then(resp => resp.json())
+      .then(imgConfirmation => {
+        console.log(imgConfirmation)
+        this.updateImageOfTheDay(imgConfirmation)
+        this.renderPage()
+      })
+  }
+
+  updateImageOfTheDay(imgConfirmation) {
+    this.status.imageOfTheDay.title = imgConfirmation.title
+    this.status.imageOfTheDay.copyright = imgConfirmation.copyright
+    this.status.imageOfTheDay.url = imgConfirmation.url
+  }
+  renderImageOfTheDay() {
+    const IOTD = document.querySelector('.IOTD')
+    document.querySelector(
+      '.image-title'
+    ).textContent = this.status.imageOfTheDay.title
+    document.querySelector('.copyright').textContent =
+      this.status.imageOfTheDay.copyright || 'no copyright'
+    IOTD.style.backgroundImage = `url(${this.status.imageOfTheDay.url})`
+  }
+  renderNextLaunches() {
+    // Need to render the upcoming launches to the screen
+    // Remove the previous launch information
+    // Add upcoming launch information
+    // Call to render page to update the information displayed
+  }
+  startCountdown() {
+    // Create count down clock to launch
+    // Provide current launch info time and count back
+    // Provide accurate times per timezone
+  }
+  goToNextLaunch() {
+    // Upon button click load next launch
+    // Call for the next indexed launch info
+    // Calls to load the launch info onto the page
+  }
+  goToPreviousLaunch() {
+    // Upon button click load next launch
+    // Call for the previously indexed launch info
+    // Calls to load the launch info onto the page
+  }
+
+  loadUpcomingLaunches() {
+    // Allows the launch information to be displayed onscreen
+  }
+}
+  renderPage() {
+    this.renderImageOfTheDay()
+    // Need to add is update for launch functionality
+  }
+
+const updateThePage = new updatePage()
+
+// LO: "something is loaded event"
+// LO: Events : Browser fired events
+document.addEventListener('DOMContentLoaded', () => updateThePage.loadPage())
